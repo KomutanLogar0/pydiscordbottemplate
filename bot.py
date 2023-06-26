ef59604cf64524c8570650c9eaff294422f9612b
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2023-06-26 13:30:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,17 @@ async def merhaba(msg):
   await msg.send("Merhaba!")
 #burada mesajı yanıtlamadan direk mesaj gönderiyoruz
 
+#Sunucudan kişileri atmak için
+@bot.command()
+async def kick(ctx, member = discord.Member, *,  reason = 'No reason'):
+    
+    if ctx.message.author.guild_permissions.kick_members  == True:
+        await member.kick(reason=reason)
+        await ctx.send(f'**{member}** was kicked.\nReason: _{reason}_')
+
+    elif ctx.message.author.guild_permissions.kick_members  == False:
+        await ctx.channel.send('You do not have permissions to do that.')
+
 bot.run("Buraya botun tokenini yazın")
 #tokeni bulmak için https://discord.com/developers kısmına girin. 
 
