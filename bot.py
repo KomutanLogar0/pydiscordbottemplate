9ea880d364778af25551f896e9b89acf5b5ae8d5
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2023-06-26 13:02:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,32 @@
 import discord
 from discord.ext import commands
 
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="mn!", intents=intents)
+#prefix 
+bot = commands.Bot(command_prefix="!", intents=intents)
 
+#Bot online olduğu zaman print fonksiyonu ile bunu belirtelim
+@bot.event
+async def on_ready():
+  print("Burayı kişileştirebilirsiniz.")
+
+#Gelen mesajları algılama ve yanıtlama başlarında prefix olması lazım
+@bot.command()
+async def sa(msg):
+  await msg.reply("as")
+#Biz burada "reply" kullanarak mesajı yanıtlayarak gönderdik
+
+@bot.command()
+async def merhaba(msg):
+  await msg.send("Merhaba!")
+#burada mesajı yanıtlamadan direk mesaj gönderiyoruz
+
+bot.run("Buraya botun tokenini yazın")
+
+
+
+  
+  
+  
